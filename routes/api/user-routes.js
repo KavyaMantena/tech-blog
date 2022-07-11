a2c1760443396bef956585b059bbd7a95f714a08
Kavya Mantena <newtechkn@kavyas-air.lan>
2022-07-11 11:18:02 -0500

    added password bcrypt

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -17,18 +17,21 @@ router.get("/:id", (req, res) => {
     where: {
       id: req.params.id,
     },
-  })
-    .then((dbUserData) => {
-      if (!dbUserData) {
-        res.status(404).json({ message: "No user found with this id" });
-        return;
-      }
-      res.json(dbUserData);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+  });
+  then((dbUserData) => {
+    if (!dbUserData) {
+      res.status(400).json({ message: "No user with that username!" });
+      return;
+    }
+
+    const validPassword = dbUserData.checkPassword(req.body.password);
+    if (!validPassword) {
+      res.status(400).json({ message: "Incorrect password!" });
+      return;
+    }
+
+    res.json({ user: dbUserData, message: "You are now logged in!" });
+  });
 });
 
 //POST /api/users
@@ -45,15 +48,32 @@ router.post("/", (req, res) => {
     });
 });
 
+router.post("/login", (req, res) => {
+  //expects {username: , and password: 'atleast length of 4}
+  User.findOne({
+    where: {
+      username: req.body.username,
+    },
+  }).then((dbUserData) => {
+    if (!dbUserData) {
+      res.status(400).json({ message: "No user with that username!" });
+      return;
+    }
+    res.json({ user: dbUserData });
+    //verify user
+  });
+});
+
 //PUT /api/users/1
 router.put("/:id", (req, res) => {
   User.update(res.body, {
+    individualHooks: true,
     where: {
       id: req.params.id,
     },
   })
     .then((dbUserData) => {
-      if (!dbUserData[0]) {
+      if (!dbUserData) {
         res.status(404).json({ message: "No user found with this id" });
         return;
       }
